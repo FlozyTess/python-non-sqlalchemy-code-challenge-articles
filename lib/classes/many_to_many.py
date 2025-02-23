d2fb4d34b50ed89d868f1689908f3af3be5db478
FlozyTess <cecilrency23@gmail.com>
2025-02-24 01:27:29 +0300

    updated class article

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,11 +1,49 @@
 class Article:
+    all = [] # we use this to store all the articles
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all.append(self)  # to store each article in a list
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str) or len(value.strip()) == 0:
+            raise ValueError("Title must be a non-empty string")
+        self._title = value
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise TypeError("Author must be of type Author")
+        self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        if not isinstance(value, Magazine):
+            raise TypeError("Magazine must be of type Magazine")
+        self._magazine = value
+
+    @classmethod
+    def clear_all(cls):
+        """Helper method to reset all articles before testing"""
+        cls.all = []
         
 class Author:
     def __init__(self, name):
+        if not isinstance(name, str) or len(name) < 1:
+            raise ValueError("Name must be a non-empty string")
         self.name = name
 
     def articles(self):
